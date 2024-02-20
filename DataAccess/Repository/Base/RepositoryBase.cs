11d3113072c351a5cc66d0d2687c30f829f366aa
Tugay <Tugay@DESKTOP-8EHVPE9>
2024-02-20 23:59:09 +0300

    repositorybase

---
+++ DataAccess/Repository/Base/RepositoryBase.cs
@@ -0,0 +1,67 @@
+﻿using DataAccess.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
+using Models.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository.Base
+{
+    public class RepositoryBase<TModel, TContext> : IRepositoryBase<TModel>
+                                                    where TModel : BaseModel
+                                                    where TContext : DbContext, new()
+    {
+        public void Add(TModel model)
+        {
+            using (TContext context = new TContext())
+            {
+                var addedModel = context.Entry(model);
+                addedModel.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TModel model)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedModel = context.Entry(model);
+                deletedModel.State = EntityState.Deleted;
+                context.SaveChanges();
+
+            }
+        }
+
+        public TModel Get(Expression<Func<TModel, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TModel>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TModel> GetAll(Expression<Func<TModel, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null
+                    ? context.Set<TModel>().ToList()
+                    : context.Set<TModel>().Where(filter).ToList();
+
+            }
+        }
+
+        public void Update(TModel model)
+        {
+            using (TContext context = new TContext())
+            {
+                var updatedModel = context.Entry(model);
+                updatedModel.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ DataAccess/Repository/Interface/IRepositoryBase.cs
@@ -0,0 +1,19 @@
+﻿using Models.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository.Interface
+{
+    public interface IRepositoryBase<TModel> where TModel : BaseModel
+    {
+        List<TModel> GetAll(Expression<Func<TModel, bool>> filter = null);
+        TModel Get(Expression<Func<TModel, bool>> filter);
+        void Add(TModel model);
+        void Update(TModel model);
+        void Delete(TModel model);
+    }
+}
