8fb88c5b2fb55001bb3466ca1851b76bc59a0d5a
Tugay <Tugay@DESKTOP-8EHVPE9>
2024-02-21 20:10:58 +0300

    context

--- DataAccess/Context/IOContext.cs
+++ DataAccess/Context/IOContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Models.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,21 @@ namespace DataAccess.Context
 {
     public class IOContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-8EHVPE9\\SQLEXPRESS;Database=IOProject;Trusted_Connection=True; Encrypt=False;");
+        }
+
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasKey(a => a.Id);
+            modelBuilder.Entity<Instructor>().HasKey(a => a.Id);
+            modelBuilder.Entity<Course>().HasKey(a => a.Id);
+        }
     }
 }
