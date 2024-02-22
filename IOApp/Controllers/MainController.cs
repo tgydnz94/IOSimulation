9425b87bd530d1361a301967e7e3b8a14ceb5330
Tugay <Tugay@DESKTOP-8EHVPE9>
2024-02-22 21:26:56 +0300

    card

---
+++ IOApp/Controllers/MainController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IOApp.Controllers
+{
+    public class MainController : Controller
+    {
+        public MainController()
+        {
+                
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- IOApp/Program.cs
+++ IOApp/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Main}/{action=Index}"
+    );
 
 app.Run();
